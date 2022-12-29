6c301e09673762a7f26a22f864ca4fef2b4b3001
Piotr Kowalski <piecioshka@gmail.com>
2022-12-30 00:19:20 +0100

    Links

--- scripts/core/app.js
+++ scripts/core/app.js
@@ -1,6 +1,6 @@
 /**
  * @author Piotr Kowalski <piecioshka@gmail.com>
- * @see http://piecioshka.pl/projects/mda-graphs/
+ * @see https://piecioshka.github.io/mda-graphs/
  * @licence The MIT License {@link http://piecioshka.mit-license.org/}
  */
 define([
